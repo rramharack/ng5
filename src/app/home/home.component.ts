0601c298f72341efbbcdb08d96862fe6010b2dab
Ron. R <rramharack@gmail.com>
2018-05-23 14:07:57 -0400

    Step 7 (Services) - Completed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { trigger, style, transition, animate, keyframes, query, stagger } from '@angular/animations';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-home',
@@ -33,23 +34,27 @@ export class HomeComponent implements OnInit {
 
   itemCount: number;
   btnText: string = 'Add an Item';
-  goalText: string;
-  goals = [ 'My first life goal',  'I want to climb a mountain',  'Go ice skiing'];
+  goalText: string = 'My first life goal';
+  goals = [];
 
-  constructor() { }
+  constructor(private _data: DataService) { }
 
   ngOnInit() {
+    this._data.goal.subscribe(res => this.goals = res);
     this.itemCount = this.goals.length;
+    this._data.changeGoal(this.goals);
   }
 
   addItem(){
     this.goals.push(this.goalText);
     this.goalText = '';
     this.itemCount = this.goals.length;
+    this._data.changeGoal(this.goals);
   }
 
   removeItem(i){
     this.goals.splice(i, 1);
     this.itemCount = this.goals.length;
+    this._data.changeGoal(this.goals);
   }
 }
