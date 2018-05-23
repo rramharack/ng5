498962c193920ea5c11c3ffc249976ae3f1c4268
Ron. R <rramharack@gmail.com>
2018-05-23 13:18:56 -0400

    Working Links - (6. Routing) - Partial (2:20)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
